af1382a436b5a579a0236f819c12f7b75fb9be01
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-20 23:20:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 def valid_move? (board, index)
+  if position_taken?(board,index) || index < 0 || index > 8
+    return false
+  elsif !position_taken?(board, index)
+    return true
+  end
+end
 end
 
 
